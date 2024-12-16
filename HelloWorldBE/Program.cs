89b99c42c813cc013c2d1bc74ca32961b1eb0e5d
Botan Elveren <botanelveren@gmail.com>
2024-12-16 13:27:09 +0300

    Hello World Patika Edition
    İlk uygulamam

---
+++ HelloWorldBE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldBE
+{ 
+class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Botan Elveren\nPatika'nın verdiği profesyonel güven ve alanında uzman ve özenli ekibi için bootcamp'deyim.");
+
+        }
+
+    }
+}
